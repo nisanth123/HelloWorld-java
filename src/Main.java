d24f35750f332c3c49cd0832a287bf6aa441ead9
sandeepprutech <159427676+sandeepprutech@users.noreply.github.com>
2024-02-13 15:18:21 +0530

    New1

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("THIS IS SANDEEP T");
+        System.out.println("1234567890");
+    }
+}
